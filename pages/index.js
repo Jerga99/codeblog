1c80150956314cafb67394cd3528480c3f155bd1
Filip Jerga <jerga99@gmail.comm>
2020-05-29 09:41:45 +0200

    6 Get blogs

--- pages/index.js
+++ pages/index.js
@@ -5,12 +5,15 @@ import AuthorIntro from 'components/AuthorIntro';
 import CardItem from 'components/CardItem';
 import CardListItem from 'components/CardListItem';
 
-export default function Home() {
+import { getAllBlogs } from 'lib/api';
 
+export default function Home({blogs}) {
+  debugger
   return (
     <PageLayout>
       <AuthorIntro />
       <hr/>
+      {JSON.stringify(blogs)}
       <Row className="mb-5">
         <Col md="10">
           <CardListItem />
@@ -21,4 +24,16 @@ export default function Home() {
       </Row>
     </PageLayout>
   )
+}
+
+// This function is called during the build (build time)
+// Provides props to your page
+// It will create static page
+export async function getStaticProps() {
+  const blogs = await getAllBlogs();
+  return {
+    props: {
+      blogs
+    }
+  }
 }
