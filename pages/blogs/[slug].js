e2288b0f8ca4d6155243a4058c27c6711ef5ff6f
Filip Jerga <jerga99@gmail.comm>
2020-05-31 13:52:36 +0200

    15 Get Static Paths

--- pages/blogs/[slug].js
+++ pages/blogs/[slug].js
@@ -3,6 +3,7 @@ import PageLayout from 'components/PageLayout';
 import { getBlogBySlug } from 'lib/api';
 
 const BlogDetail = ({blog}) => {
+  console.log('Displaying page');
   return (
     <PageLayout>
       <h1>Hello Detail Page - {blog?.slug}</h1>
@@ -10,10 +11,27 @@ const BlogDetail = ({blog}) => {
   )
 }
 
-export async function getServerSideProps({params}) {
+export async function getStaticProps({params}) {
+  console.log('Fetching blog by', params.slug)
   const blog = await getBlogBySlug(params.slug);
   return {
     props: {blog}
+  }
+}
+
+export function getStaticPaths() {
+  console.log('Getting paths for every page')
+  return {
+    paths: [{
+      params: {slug: 'my-second-blog'}
+    },
+    {
+      params: {slug: 'my-third-blog'}
+    },
+    {
+      params: {slug: 'my-first-blog'}
+    }],
+    fallback: false
   }
 }
 
