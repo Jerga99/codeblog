5fdd35b0ccf05ac8c54fbeab8e100c93e24ac44c
Filip Jerga <jerga99@gmail.comm>
2020-05-31 11:55:58 +0200

    12 Font + Brand Style

--- components/PageLayout.js
+++ components/PageLayout.js
@@ -1,21 +1,27 @@
 
 import { Container } from 'react-bootstrap';
+import Head from 'next/head';
 import Navbar from './Navbar';
 
 export default function PageLayout({children, className}) {
   return (
-    <Container>
-      <Navbar />
-      <div className={`page-wrapper ${className}`}>
-        {children}
-      </div>
-      <footer className="page-footer">
-        <div>
-          <a href="#">courses</a>{' | '}
-          <a href="#">github</a>{' | '}
-          <a href="#">facebook</a>
+    <>
+      <Head>
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;700;900&display=swap" rel="stylesheet" />
+      </Head>
+      <Container>
+        <Navbar />
+        <div className={`page-wrapper ${className}`}>
+          {children}
         </div>
-      </footer>
-    </Container>
+        <footer className="page-footer">
+          <div>
+            <a href="#">courses</a>{' | '}
+            <a href="#">github</a>{' | '}
+            <a href="#">facebook</a>
+          </div>
+        </footer>
+      </Container>
+    </>
   )
 }
