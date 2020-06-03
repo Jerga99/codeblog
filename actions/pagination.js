45241413c287cf48c0e5df99d77d31f448e1cd6c
Filip Jerga <jerga99@gmail.comm>
2020-06-03 15:18:47 +0200

    33 SWR Pagination

---
+++ actions/pagination.js
@@ -0,0 +1,57 @@
+
+
+import { useSWRPages } from 'swr';
+import { useGetBlogs } from 'actions';
+import { Col } from 'react-bootstrap';
+import CardItem from 'components/CardItem';
+import CardListItem from 'components/CardListItem';
+
+export const useGetBlogsPages = ({blogs: initialData, filter}) => {
+
+  return useSWRPages(
+    'index-page',
+    ({offset, withSWR}) => {
+      const { data: blogs } =  withSWR(useGetBlogs(initialData));
+
+      if (!blogs) { return 'Loading...'}
+
+      return blogs
+        .map(blog =>
+        filter.view.list ?
+          <Col key={`${blog.slug}-list`} md="9">
+            <CardListItem
+              author={blog.author}
+              title={blog.title}
+              subtitle={blog.subtitle}
+              date={blog.date}
+              link={{
+                href: '/blogs/[slug]',
+                as: `/blogs/${blog.slug}`
+              }}
+            />
+          </Col>
+          :
+          <Col key={blog.slug} md="4">
+            <CardItem
+              author={blog.author}
+              title={blog.title}
+              subtitle={blog.subtitle}
+              date={blog.date}
+              image={blog.coverImage}
+              link={{
+                href: '/blogs/[slug]',
+                as: `/blogs/${blog.slug}`
+              }}
+            />
+          </Col>
+        )
+    },
+    // here you will compute offset that will get passed into previous callback function with 'withSWR'
+    // SWR: data you will get from 'withSWR' function
+    // index: number of current page
+    (SWR, index) => {
+      return 0;
+    },
+    [filter]
+  )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,38 @@
 
 import { useState } from 'react';
 
-import { Row, Col } from 'react-bootstrap';
+import { Row } from 'react-bootstrap';
 import PageLayout from 'components/PageLayout';
 import AuthorIntro from 'components/AuthorIntro';
-import CardItem from 'components/CardItem';
-import CardListItem from 'components/CardListItem';
 import FilteringMenu from 'components/FilteringMenu';
 
+import { useGetBlogsPages } from 'actions/pagination';
 import { getAllBlogs } from 'lib/api';
-import { useGetBlogs } from 'actions';
 
-export default function Home({blogs: initialData}) {
+export default function Home({blogs}) {
   const [filter, setFilter] = useState({
     view: { list: 0 }
   });
 
-  const { data: blogs, error } = useGetBlogs(initialData);
+  const {
+    pages,
+    isLoadingMore,
+    isReachingEnd,
+    loadMore
+  } = useGetBlogsPages({blogs, filter});
 
   return (
     <PageLayout>
       <AuthorIntro />
       <FilteringMenu
         filter={filter}
-        onChange={(option, value) => {
-          debugger
-          setFilter({...filter, [option]: value});
-        }}
+        onChange={(option, value) =>
+          setFilter({...filter, [option]: value})
+        }
       />
       <hr/>
       <Row className="mb-5">
-        { blogs.map(blog =>
-          filter.view.list ?
-            <Col key={`${blog.slug}-list`} md="9">
-              <CardListItem
-                author={blog.author}
-                title={blog.title}
-                subtitle={blog.subtitle}
-                date={blog.date}
-                link={{
-                  href: '/blogs/[slug]',
-                  as: `/blogs/${blog.slug}`
-                }}
-              />
-            </Col>
-            :
-            <Col key={blog.slug} md="4">
-              <CardItem
-                author={blog.author}
-                title={blog.title}
-                subtitle={blog.subtitle}
-                date={blog.date}
-                image={blog.coverImage}
-                link={{
-                  href: '/blogs/[slug]',
-                  as: `/blogs/${blog.slug}`
-                }}
-              />
-            </Col>
-          )
-        }
+        {pages}
       </Row>
     </PageLayout>
   )
