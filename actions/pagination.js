f9b26b71c0396c66ae84eee71b9bd06bf3eaec61
Filip Jerga <jerga99@gmail.comm>
2020-06-04 11:36:13 +0200

    37 Fix fetching more blogs

--- actions/pagination.js
+++ actions/pagination.js
@@ -6,15 +6,16 @@ import { Col } from 'react-bootstrap';
 import CardItem from 'components/CardItem';
 import CardListItem from 'components/CardListItem';
 
-export const useGetBlogsPages = ({blogs: initialData, filter}) => {
+export const useGetBlogsPages = ({blogs, filter}) => {
 
   return useSWRPages(
     'index-page',
     ({offset, withSWR}) => {
-      const { data: blogs } =  withSWR(useGetBlogs({offset}));
-      if (!blogs) { return 'Loading...'}
+      let initialData = !offset && blogs;
+      const { data: paginatedBlogs } =  withSWR(useGetBlogs({offset}, initialData));
+      if (!paginatedBlogs) { return 'Loading...'}
 
-      return blogs
+      return paginatedBlogs
         .map(blog =>
         filter.view.list ?
           <Col key={`${blog.slug}-list`} md="9">
