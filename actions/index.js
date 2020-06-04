ae7637f789ca552913dbc855589a8ed7b2c40c2f
Filip Jerga <jerga99@gmail.comm>
2020-06-04 11:08:51 +0200

    35 Passing Offset

--- actions/index.js
+++ actions/index.js
@@ -5,6 +5,6 @@ const fetcher = url => fetch(url).then(res => res.json())
 
 export const useGetHello = () => useSWR('/api/hello', fetcher);
 
-export const useGetBlogs = (initialData) => {
-  return useSWR(`/api/blogs`, fetcher, {initialData})
+export const useGetBlogs = ({offset}, initialData) => {
+  return useSWR(`/api/blogs?offset=${offset || 0}`, fetcher, {initialData})
 }
--- actions/pagination.js
+++ actions/pagination.js
@@ -11,8 +11,7 @@ export const useGetBlogsPages = ({blogs: initialData, filter}) => {
   return useSWRPages(
     'index-page',
     ({offset, withSWR}) => {
-      debugger
-      const { data: blogs } =  withSWR(useGetBlogs(initialData));
+      const { data: blogs } =  withSWR(useGetBlogs({offset}));
       if (!blogs) { return 'Loading...'}
 
       return blogs
@@ -50,8 +49,8 @@ export const useGetBlogsPages = ({blogs: initialData, filter}) => {
     // SWR: data you will get from 'withSWR' function
     // index: number of current page
     (SWR, index) => {
-      // TODO: Compute offset here!
-      return 0;
+      if (SWR.data && SWR.data.length === 0) { return null; }
+      return (index + 1) * 3;
     },
     [filter]
   )
