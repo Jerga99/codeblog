914ca6d1142b6bcbd5838f9e28276bce1438d541
Filip Jerga <jerga99@gmail.comm>
2020-06-04 11:18:06 +0200

    36 Fetch more blogs, extreact offset in api route

--- pages/api/blogs.js
+++ pages/api/blogs.js
@@ -2,6 +2,7 @@
 import { getAllBlogs } from 'lib/api';
 
 export default async function getBlogs(req, res) {
-  const data = await getAllBlogs();
+  const offset = parseInt((req.query.offset || 0), 10);
+  const data = await getAllBlogs({offset});
   res.status(200).json(data);
 }
