5cb1f23b0cc3c74ebb480e9addf923db874bfc6b
Filip Jerga <jerga99@gmail.comm>
2020-06-07 12:57:14 +0200

    48 preview api route

---
+++ pages/api/preview.js
@@ -0,0 +1,20 @@
+
+import { getBlogBySlug } from 'lib/api';
+
+export default async function enablePreview(req, res) {
+
+  if (req.query.secret !== process.env.SANITY_PREVIEW_SECRET || !req.query.slug) {
+    return res.status(401).json({message: 'Invalid token'})
+  }
+
+  const blog = await getBlogBySlug(req.query.slug);
+
+  if (!blog) {
+    return res.status(401).json({message: 'Invalid Slug!'})
+  }
+  // setPreviewData will set cookies into you browsert
+  // __prerender_bypass __next_preview_data
+  res.setPreviewData({message: 'Hello World'});
+  res.writeHead(307, { Location: `/blogs/${blog.slug}`})
+  res.end();
+}
